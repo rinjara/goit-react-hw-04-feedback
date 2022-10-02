af41a1f6e204faa6b3a83e7ace587e22f4f0626d
Krystyna Sharhorodska <rinjara@gmail.com>
2022-10-03 01:03:00 +0300

    Hw start, stoped on implementing handleClick function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+import { Statistics } from './Statistics/Statistics';
+// import PropTypes from 'prop-types';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleStatsIncrement = event => {
+    this.setState(prevState => {
+      const value = event.currentTarget.value;
+      console.log(value);
+      return {};
+    });
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good, neutral, bad } = this.state;
+    return (good / (good + neutral + bad)) * 100;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const {
+      countTotalFeedback,
+      countPositiveFeedbackPercentage,
+      handleStatsIncrement,
+    } = this;
+    const total = countTotalFeedback();
+    const positivePercentage = countPositiveFeedbackPercentage();
+    const options = Object.keys(this.state);
+
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={handleStatsIncrement}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        </Section>
+      </>
+    );
+  }
+}
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,11 @@
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <div>
+      {options.map(option => (
+        <button type="button" key={option} onClick={() => onLeaveFeedback()}>
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
